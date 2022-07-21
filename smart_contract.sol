42bf1ff5a889a95e0ec5ecda4152853f16093c98
Franco <fcerino@mi.unc.edu.ar>
2022-07-21 20:17:56 -0300

    uploading smart contract

---
+++ smart_contract.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FirstContract{
+    uint cedula;
+    uint edad;
+    string nombre;
+
+    function setData(uint _cedula, string memory _nombre, uint _edad)public{
+        cedula = _cedula;
+        nombre = _nombre;
+        edad = _edad;
+    }
+
+    function getId() public view returns(uint){
+        return cedula;
+    }
+
+    function getName() public view returns(string memory){
+        return nombre;
+    }
+
+    function getAge() public view returns(uint){
+        return edad;
+    }
+
+    function getFullData() public  view returns(uint, string memory, uint){
+        return(edad,nombre,cedula);
+    }
+}
